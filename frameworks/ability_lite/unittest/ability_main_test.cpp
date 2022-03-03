57323d9cf20beee3b27f158ce76acf7490469f08
吴珞风 <wuluofeng@huawei.com>
2022-03-03 09:01:08 +0000

    1

--- frameworks/ability_lite/unittest/ability_main_test.cpp
+++ frameworks/ability_lite/unittest/ability_main_test.cpp
@@ -16,19 +16,10 @@
 #include <gtest/gtest.h>
 #include "../../../interfaces/innerkits/abilitymgr_lite/ability_main.h"
 
-
-#define TDD_CASE_BEGIN() SetUp()
-#define TDD_CASE_END() TearDown()
-
-#define HC_SUCCESS 0x00000000
-
 using namespace testing::ext;
 
-
-namespace OHOS
-{
-    class AbilityMainTest : public testing::Test
-    {
+namespace OHOS {
+    class AbilityMainTest : public testing::Test {
     public:
         void SetUp() override
         {
@@ -39,19 +30,16 @@ namespace OHOS
         {
             printf("AbilityMainTest TearDown");
         }
-
         void AbilityMainTest001();
-
     };
 
     void AbilityMainTest::AbilityMainTest001()
     {
         printf("AbilityMainTest setup");
         TDD_CASE_BEGIN();
-        const char *token = "com.huawei.launcher_BM70W1/aVSbkx+uI/WT/mO9NqmtEBx9esLAogYAid75/gTMpKWqrNUT5hS9Cj"
-                              "Bq6kt1OcxgZzdCJ4HuVyS4dP8w=";
+        const char *token = "launcher";
         auto ret = AbilityMain(token);
-        EXPECT_TRUE(ret == HC_SUCCESS);
+        EXPECT_TRUE(ret == 0);
         printf("--------AbilityMainTest001  end--------");
         TDD_CASE_END();
     }
