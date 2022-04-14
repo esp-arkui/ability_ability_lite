ce89fde20a354f047615ca6e46ed20d3ecee2f93
吴珞风 <wuluofeng@huawei.com>
2022-04-14 03:19:55 +0000

    Signed-off-by: wuluofeng <wuluofeng@huawei.com>

--- interfaces/kits/ability_lite/ability_loader.h
+++ interfaces/kits/ability_lite/ability_loader.h
@@ -129,9 +129,9 @@ private:
 #define REGISTER_AS(className)                                                                \
     __attribute__((constructor)) void RegisterAS##className() {                               \
         do{\
-        AbilityLoader::GetInstance().RegisterAbilitySlice(#className, []()->AbilitySlice* {   \
-            return new className;                                                             \
-        });  \
+            AbilityLoader::GetInstance().RegisterAbilitySlice(#className, []()->AbilitySlice* {   \
+                return new className;                                                             \
+            });  \
         }while(0)                                                                                 \
     }
 #endif
