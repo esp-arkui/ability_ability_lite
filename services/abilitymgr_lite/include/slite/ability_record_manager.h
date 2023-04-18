193bb7ae853af19d9e94b1cbf185be519eee3033
songjindian <songjindian1@huawei.com>
2023-04-18 10:14:01 +0800

    bms添加查询接口
    Signed-off-by: songjindian <songjindian1@huawei.com>
    Change-Id: I96afabaa31acf6bee4d91f1c4d8e4c9dd139fad7

--- services/abilitymgr_lite/include/slite/ability_record_manager.h
+++ services/abilitymgr_lite/include/slite/ability_record_manager.h
@@ -22,6 +22,7 @@
 #include "ability_record.h"
 #include "ability_stack.h"
 #include "adapter.h"
+#include "bms_helper.h"
 #include "js_ability_thread.h"
 #include "nocopyable.h"
 #include "want.h"
@@ -30,12 +31,6 @@
 
 namespace OHOS {
 namespace AbilitySlite {
-struct AbilitySvcInfo {
-    char *bundleName;
-    char *path;
-    void *data;
-    uint16_t dataLength;
-};
 
 class AbilityRecordManager : public NoCopyable {
 public:
@@ -111,7 +106,6 @@ private:
 
     int32_t ForceStopBundleInner(uint16_t token);
 
-    bool IsValidAbility(AbilityInfo *abilityInfo);
     bool IsLauncher(const char *bundleName);
 
     Want *CreateWant(const AbilityRecord *record);
--- services/abilitymgr_lite/src/slite/ability_record_manager.cpp
+++ services/abilitymgr_lite/src/slite/ability_record_manager.cpp
@@ -72,20 +72,6 @@ void AbilityRecordManager::StartLauncher()
     (void) SchedulerLifecycleInner(record, SLITE_STATE_FOREGROUND);
 }
 
-bool AbilityRecordManager::IsValidAbility(AbilityInfo *abilityInfo)
-{
-    if (abilityInfo == nullptr) {
-        return false;
-    }
-    if (abilityInfo->bundleName == nullptr || abilityInfo->srcPath == nullptr) {
-        return false;
-    }
-    if (strlen(abilityInfo->bundleName) == 0 || strlen(abilityInfo->srcPath) == 0) {
-        return false;
-    }
-    return true;
-}
-
 bool AbilityRecordManager::IsLauncher(const char *bundleName)
 {
     size_t len = strlen(bundleName);
@@ -160,33 +146,19 @@ int32_t AbilityRecordManager::StartAbility(const Want *want)
         HILOG_ERROR(HILOG_MODULE_AAFWK, "Ability Service AbilitySvcInfo is null");
         return PARAM_NULL_ERROR;
     }
-
     if (IsLauncher(bundleName)) {
         // Launcher
         info->bundleName = Utils::Strdup(bundleName);
         info->path = nullptr;
     } else {
         // JS APP
-#ifdef _MINI_BMS_
-        AbilityInfo abilityInfo = { nullptr, nullptr };
-        QueryAbilityInfo(want, &abilityInfo);
-        if (!(BMSHelper::GetInstance().IsNativeApp(bundleName) || IsValidAbility(&abilityInfo))) {
-            APP_ERRCODE_EXTRA(EXCE_ACE_APP_START, EXCE_ACE_APP_START_UNKNOWN_BUNDLE_INFO);
-            ClearAbilityInfo(&abilityInfo);
+        uint8_t queryRet = BMSHelper::GetInstance().QueryAbilitySvcInfo(want, info);
+        if (queryRet != ERR_OK) {
             AdapterFree(info);
-            HILOG_ERROR(HILOG_MODULE_AAFWK, "Ability Service returned bundleInfo is not valid");
-            return PARAM_NULL_ERROR;
+            HILOG_ERROR(HILOG_MODULE_AAFWK, "Failed to query AbilitySvcInfo");
+            return PARAM_CHECK_ERROR;
         }
-        info->bundleName = OHOS::Utils::Strdup(abilityInfo.bundleName);
-        info->path = OHOS::Utils::Strdup(abilityInfo.srcPath);
-        ClearAbilityInfo(&abilityInfo);
-#else
-        info->bundleName = Utils::Strdup(bundleName);
-        // Here users assign want->data with js app path.
-        info->path = Utils::Strdup((const char *)want->data);
-#endif
     }
-
     info->data = OHOS::Utils::Memdup(want->data, want->dataLength);
     info->dataLength = want->dataLength;
     auto ret = StartAbility(info);
